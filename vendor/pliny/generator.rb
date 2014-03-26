16af775198c7b6f34e44cd892c546d74e270a3fa
Pedro Belo <pedro@heroku.com>
2014-03-26 11:31:14 -0700

    move all this generate crap to a class

---
+++ vendor/pliny/generator.rb
@@ -0,0 +1,50 @@
+require "erb"
+require "fileutils"
+require "ostruct"
+
+module Pliny
+  class Generator
+    attr_accessor :type
+
+    def self.run(args)
+      new(args).run!
+    end
+
+    def initialize(args)
+      @type = args.first
+    end
+
+    def run!
+      case @type
+      when "model"
+        name = ARGV[1] || abort("Missing model name")
+        create_model(name)
+      else
+        abort("Don't know how to generate #{@type}.")
+      end
+    end
+
+    def create_model(name)
+      class_name = name.capitalize
+      file_name  = name.downcase
+
+      model = "./lib/models/#{file_name}.rb"
+      render_template("model.erb", model, class_name: class_name)
+      puts "created model file #{model}"
+
+      migration = "./db/migrate/#{Time.now.to_i}_create_#{file_name}.rb"
+      render_template("migration.erb", migration, table_name: "#{file_name}s")
+      puts "created migration #{migration}"
+    end
+
+    def render_template(template_file, destination_path, vars)
+      template_path = "./vendor/pliny/templates/#{template_file}"
+      template = ERB.new(File.read(template_path))
+      FileUtils.mkdir_p(File.dirname(destination_path))
+      File.open(destination_path, "w") do |f|
+        context = OpenStruct.new(vars)
+        f.puts template.result(context.instance_eval { binding })
+      end
+    end
+  end
+end
