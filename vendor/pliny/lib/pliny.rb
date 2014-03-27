83e6d05adcb5477ecee9a84368dbaef37ccb0ec7
Pedro Belo <pedro@heroku.com>
2014-03-27 11:56:52 -0700

    missing require

--- vendor/pliny/lib/pliny.rb
+++ vendor/pliny/lib/pliny.rb
@@ -2,6 +2,7 @@ require "sinatra"
 
 require "pliny/generator"
 require "pliny/log"
+require "pliny/request_store"
 require "pliny/utils"
 require "pliny/middleware/cors"
 require "pliny/middleware/request_id"
