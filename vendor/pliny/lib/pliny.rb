bcf68a32de00f27ad700e2121d3a3d145fdcdc64
Brandur <brandur@mutelight.org>
2014-03-26 17:27:17 -0700

    Add Pliny::RequestStore module

--- vendor/pliny/lib/pliny.rb
+++ vendor/pliny/lib/pliny.rb
@@ -1,6 +1,8 @@
 require "sinatra"
+
 require "pliny/generator"
 require "pliny/log"
+require "pliny/request_store"
 require "pliny/utils"
 require "pliny/middleware/cors"
 require "pliny/middleware/request_id"
