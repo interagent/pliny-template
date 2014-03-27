e906c06de90004a1a88b4a6e366f5b1b9f6cf5ea
Brandur <brandur@mutelight.org>
2014-03-27 14:07:52 -0700

    Bundle instruments into Pliny
    Instruments are slightly more effective in from within Sinatra because
    we can return a route signature.

--- lib/endpoints/base.rb
+++ lib/endpoints/base.rb
@@ -1,6 +1,7 @@
 module Endpoints
   # The base class for all Sinatra-based endpoints. Use sparingly.
   class Base < Sinatra::Base
+    register Pliny::Extensions::Instruments
     register Sinatra::Namespace
 
     not_found do
--- lib/routes.rb
+++ lib/routes.rb
@@ -1,14 +1,13 @@
 Routes = Rack::Builder.new do
   use Honeybadger::Rack
-  use Rack::Instruments
   use Pliny::Middleware::CORS
   use Pliny::Middleware::RequestID
   use Pliny::Middleware::RequestStore, store: Pliny::RequestStore
 
   use Sinatra::Router do
     # mount all individual Sinatra apps here
-
-    # root app; but will also handle some defaults like 404
-    run Endpoints::Root
   end
+
+  # root app; but will also handle some defaults like 404
+  run Endpoints::Root
 end
