6ef28a85f6fe1d582f03503dd2cc6e84c891d7f5
Clint Shryock <clint@ctshryock.com>
2014-04-02 08:23:26 -0500

    Add Sinatra::Reloader for development
    This adds Sinatra::Reloader enables it when in development. Sinatra::Reloader
    will automatically reload classes/files on change in development, so you don't
    need to start/stop the server to pick up changes.

--- lib/endpoints/base.rb
+++ lib/endpoints/base.rb
@@ -1,8 +1,13 @@
+require 'sinatra/reloader'
 module Endpoints
   # The base class for all Sinatra-based endpoints. Use sparingly.
   class Base < Sinatra::Base
     register Pliny::Extensions::Instruments
     register Sinatra::Namespace
+
+    configure :development do
+      register Sinatra::Reloader
+    end
 
     not_found do
       content_type :json
