473dd48a0626214b8a991cd7e5c2d51014988077
Brandur <brandur@mutelight.org>
2014-04-04 12:50:30 -0700

    Env vars for Puma thread min/max/workers

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,9 @@
 environment (ENV["RACK_ENV"] || "development")
 port ENV["PORT"] || 5000
 quiet
-threads 1, 16
-workers 3
+threads Integer(ENV['PUMA_MIN_THREADS']  || 1),
+  Integer(ENV['PUMA_MAX_THREADS'] || 16)
+workers Integer(ENV['PUMA_WORKERS'] || 3)
 
 on_worker_boot do
   # force Sequel's thread pool to be refreshed
