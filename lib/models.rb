30729831ca9d9e6f19e901c6846227131162c7de
Brandur <brandur@mutelight.org>
2014-04-07 09:15:12 -0700

    Ugh, remove development `puts`

--- lib/models.rb
+++ lib/models.rb
@@ -1,4 +1,3 @@
 # Sequel requires a DB connection before any models can successfully be loaded
 # so we initialize them "late" in a separate load file.
-puts "loading models!!"
 Pliny::Utils.require_relative_glob("lib/models/**/*.rb")
