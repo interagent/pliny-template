24daec1eccf720499b02624b7ca0582ac7813b2e
Brandur <brandur@mutelight.org>
2014-04-19 20:31:13 -0700

    More idiomatic code

--- lib/app.rb
+++ lib/app.rb
@@ -3,18 +3,21 @@ module App
     Pliny::Utils.require_glob("#{App.root}/config/initializers/*.rb")
   end
 
+  def self.require!(globs)
+    globs.map { |f| App.root + "/" + f + ".rb" }.
+      each { |f| Pliny::Utils.require_glob(f) }
+  end
+
   def self.root
     @@root ||= File.expand_path("../../", __FILE__)
   end
 end
 
-[
+App.require!([
   "config/config",
   "lib/endpoints/base",
   "lib/endpoints/**/*",
   "lib/mediators/base",
   "lib/mediators/**/*",
   "lib/routes",
-].
-map { |f| App.root + "/" + f + ".rb" }.
-each { |f| Pliny::Utils.require_glob(f) }
+])
