07c1dc04daaceef6369651e6f96612fcef9926b8
Pedro Belo <pedro@heroku.com>
2014-05-09 16:57:03 -0700

    Tweak app initialization
    move lib/app to lib/initializer, load everything with that file

---
+++ lib/initializer.rb
@@ -0,0 +1,39 @@
+module Initializer
+  def self.run
+    require_config
+    initialize_database
+    require_lib
+    require_initializers
+  end
+
+  def self.require_config
+    require! "config/config.rb"
+  end
+
+  def self.initialize_database
+    Sequel.connect(Config.database_url, max_connections: Config.db_pool)
+  end
+
+  def self.require_lib
+    files = Pliny::Utils.glob("lib/**/*.rb")
+    require! files - ["lib/routes.rb"]
+    require! "lib/routes.rb"
+  end
+
+  def self.require_initializers
+    Pliny::Utils.require_glob("#{root}/config/initializers/*.rb")
+  end
+
+  def self.require!(globs)
+    globs = [globs] unless globs.is_a?(Array)
+    globs.each do |f|
+      Pliny::Utils.require_glob("#{root}/#{f}")
+    end
+  end
+
+  def self.root
+    @@root ||= File.expand_path("../../", __FILE__)
+  end
+end
+
+Initializer.run
