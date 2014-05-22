b65cdc1ed72c5bb649f18422ae915fcefb8b49ff
Mark Fine <mark.fine@gmail.com>
2014-05-21 17:48:33 -0700

    simple serializer impl.

--- lib/serializers/base.rb
+++ lib/serializers/base.rb
@@ -1,9 +1,29 @@
 class Serializers
   class Base
-    def self.structure(*args, &blk)
+    @@structures = {}
+
+    def self.structure(type, &blk)
+      @@structures["#{self.name}::#{type}"] = blk
+    end
+
+    def initialize(type)
+      @type = type
     end
 
     def serialize(object)
+      if object.is_a?(Array)
+        object.map do |item|
+          serialize_item(item, @type)
+        end
+      else
+        serialize_item(object, @type)
+      end
+    end
+
+    private
+
+    def serialize_item(item, type)
+      @@structures["#{self.name}::#{type}"].call(item)
     end
   end
 end
