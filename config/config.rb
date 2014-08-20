cafe9c11d90426949ed9faecdd15ae588db7e7c8
Will Leinweber <will@bitfission.com>
2014-08-20 13:58:08 -0700

    change to use casting config by default

--- config/config.rb
+++ config/config.rb
@@ -6,35 +6,28 @@ require "pliny/config_helpers"
 #
 # Each accessor corresponds directly to an ENV key, which has the same name
 # except upcased, i.e. `DATABASE_URL`.
-#
-# Note that *all* keys will come out as strings even if the override was a
-# different type. Make sure to typecast any values that need to be something
-# else (i.e. `.to_i`).
 module Config
-  extend Pliny::ConfigHelpers
+  extend Pliny::CastingConfigHelpers
 
   # Mandatory -- exception is raised for these variables when missing.
-  mandatory \
-    :database_url
+  mandatory :database_url, string
 
   # Optional -- value is returned or `nil` if it wasn't present.
-  optional \
-    :console_banner,
-    :placeholder,
-    :versioning_default,
-    :versioning_app_name
+  optional :console_banner,      string
+  optional :placeholder,         string
+  optional :versioning_default,  string
+  optional :versioning_app_name, string
 
-  # Override -- value is returned or the set default. Remember to typecast.
-  override \
-    db_pool:          5,
-    port:             5000,
-    puma_max_threads: 16,
-    puma_min_threads: 1,
-    puma_workers:     3,
-    rack_env:         'development',
-    raise_errors:     'false',
-    root:             File.expand_path("../../", __FILE__),
-    timeout:          45,
-    force_ssl:        'true',
-    versioning:       'false'
+  # Override -- value is returned or the set default.
+  override :db_pool,          5,    int
+  override :port,             5000, int
+  override :puma_max_threads, 16,   int
+  override :puma_min_threads, 1,    int
+  override :puma_workers,     3,    int
+  override :rack_env,         'development', string
+  override :raise_errors,     false,         bool
+  override :root,             File.expand_path("../../", __FILE__), string
+  override :timeout,          45,    int
+  override :force_ssl,        true,  bool
+  override :versioning,       false, bool
 end
--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@ require "./config/config"
 environment Config.rack_env
 port Config.port
 quiet
-threads Config.puma_min_threads.to_i, Config.puma_max_threads.to_i
-workers Config.puma_workers.to_i
+threads Config.puma_min_threads, Config.puma_max_threads
+workers Config.puma_workers
 
 on_worker_boot do
   # force Sequel's thread pool to be refreshed
